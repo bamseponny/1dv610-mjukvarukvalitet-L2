ad9b51dc1d47195ab426ffec5c6a08abf42303b6
Fredrik Eriksson <ferth09@student.lnu.se>
2022-10-11 07:12:02 +0200

    Implement the module

---
+++ module/database.js
@@ -0,0 +1,100 @@
+/**
+ * Module for the database.
+ *
+ * @author Fredrik Eriksson <ferth09@student.lnu.se>
+ */
+
+export const library = [
+  {
+    title: 'Horizon Forbidden West',
+    format: 'Playstation 5',
+    releaseYear: 2022,
+    genre: 'Action',
+    series: 'Horizon',
+    grade: 5,
+    hoursPlayed: 88
+  },
+  {
+    title: 'The Last of Us Part II',
+    format: 'Playstation 4',
+    releaseYear: 2020,
+    genre: 'Action',
+    series: 'The Last of Us',
+    grade: 4,
+    hoursPlayed: 30
+  },
+  {
+    title: 'The Last of Us Part I',
+    format: 'Playstation 4',
+    releaseYear: 2022,
+    genre: 'Action',
+    series: 'The Last of Us',
+    grade: 4,
+    hoursPlayed: 16
+  },
+  {
+    title: 'Paper Mario',
+    format: 'Nintendo 64',
+    releaseYear: 2001,
+    genre: 'RPG',
+    series: 'Paper Mario',
+    grade: 3,
+    hoursPlayed: 27
+  },
+  {
+    title: 'Eiyuden Chronicle: Rising',
+    format: 'Playstation 5',
+    releaseYear: 2022,
+    genre: 'RPG',
+    series: 'Eiyuden Chronicle',
+    grade: 3,
+    hoursPlayed: 20
+  },
+  {
+    title: 'Ratchet & Clank',
+    format: 'Playstation 4',
+    releaseYear: 2016,
+    genre: 'Platform',
+    series: 'Ratchet & Clank',
+    grade: 4,
+    hoursPlayed: 16
+  },
+  {
+    title: 'Ni no Kuni: Wrath of the White Witch',
+    format: 'Playstation 4',
+    releaseYear: 2013,
+    genre: 'RPG',
+    series: 'Ni no Kuni',
+    grade: 3,
+    hoursPlayed: 35
+  },
+  {
+    title: 'Star Wars: Knights of the Old Republic',
+    format: 'Xbox',
+    releaseYear: 2013,
+    genre: 'RPG',
+    series: 'Star Wars: Knights of the Old Republic',
+    grade: 3,
+    hoursPlayed: 88
+  },
+  {
+    title: 'Enslaved: Odyssey to the West',
+    format: 'Xbox 360',
+    releaseYear: 2010,
+    genre: 'Action',
+    series: '',
+    grade: 4,
+    hoursPlayed: 36
+  },
+  {
+    title: 'Syberia',
+    format: 'Pc',
+    releaseYear: 2002,
+    genre: 'Adventure',
+    series: '',
+    grade: 3,
+    hoursPlayed: 10
+  }
+]
+
+export default library
